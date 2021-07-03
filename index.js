6c98788eb2c102021d6059380339ff83dc6d5745
Sabine Wren <pandyaved1999@gmail.com>
2021-07-03 13:44:30 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var menuBtn = document.getElementById("menuBtn")
+var sideNav = document.getElementById("sideNav")
+var menu = document.getElementById("menu")
+
+sideNav.style.right = "-250px";
+menuBtn.onclick = function()
+{
+    if(sideNav.style.right == "-250px")
+    {
+        sideNav.style.right == "0";
+    }
+    else
+    {
+        sideNav.style.right == "-250px";
+    }
+}
